e9d2d17540118b61c954c4b60cfb2bb454b72563
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-06-15 23:37:52 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var percentage=document.getElementById('percentage');
+
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    )
+}
+
+window.addEventListener('scroll',function(){
+    var winheight= window.innerHeight || (document.documentElement || document.body).clientHeight
+    var docheight = getDocHeight()
+    var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
+    var trackLength = docheight - winheight
+    var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+    percentage.innerText=pctScrolled;
+})
